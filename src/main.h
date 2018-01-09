878bd3cd75512106dc924e87545ee8f842eafa7e
kmsheng <kmsh3ng@gmail.com>
2018-01-09 23:11:09 +0800

    add main.h file

---
+++ src/main.h
@@ -0,0 +1,50 @@
+char *lunmanP[27];
+char *lunmanT[27] = {
+    "tha skar", "bra nye", "smin drug", "snar ma", "mgo",
+    "lag", "nabs so", "rgyal", "skag", "mchu", "gre", "dbo",
+    "me bzhi", "nag pa", "sa ri", "sa ga", "lha mtshams",
+    "snron", "snrubs", "chu stod", "chu smad", "gro zhin",
+    "mon gre", "mon gru", "khrums stod", "khrums smad",
+    "nam gru"
+};
+
+char *lunmanS[27] = {
+    "Ashvini", "Bharani", "Krittika", "Rohini", "Mrigashiras",
+    "Ardra", "Punarvasu", "Pushya", "Ashlesha", "Magha",
+    "Purvaphalguni", "Uttaraphalguni", "Hasta", "Citra",
+    "Svati", "Vishakha", "Anuradha", "Jyeshtha", "Mula",
+    "Purvashadha", "Uttarashadha", "Abhijit", "Dhanishtha",
+    "Shatabhishaj", "Purvabhadrapada", "Uttarabhadrapada",
+    "Revati"
+};
+
+char *yogaP[27];
+char *yogaT[27] = {
+    "rnam sel", "mdza' bo", "tshe dang ldan pa", "skal bzang",
+    "dge byed", "shin tu 'grams", "las bzang", "'dzin byed",
+    "zug rngu", "'grams", "'phel", "brtan pa", "yongs bsnun",
+    "dga' ba", "rdo rje", "dngos grub", "phan tshun",
+    "mchog can", "yongs 'joms", "zhi ba", "grub pa",
+    "bsgrub bya", "dge ba", "dkar po", "tshangs pa",
+    "dbang po", "'khon 'dzin"
+};
+
+char *yogaS[27] = {
+    "Vishkambha", "Priti", "Ayushmat", "Saubhagya", "Shobhana",
+    "Atiganda", "Sukarman", "Dhriti", "Shula", "Ganda",
+    "Vriddhi", "Dhruva", "Vyaghata", "Harshana", "Vajra",
+    "Siddhi", "Vyatipata", "Variyas", "Parigha", "Shiva",
+    "Siddha", "Sadhya", "Shubha", "Shukla", "Brahman",
+    "Indra", "Vaidhriti"
+};
+
+char *byedP[11];
+char *byedT[11] = {
+    "gdab pa", "byis pa", "rigs can", "til rdung",
+     "khyim skyes", "tshong ba", "vishti", "mi sdug pa",
+     "bkra shis", "rkang bzhi", "klu"
+};
+char *byedS[11] = {
+    "Vava", "Valava", "Kaulava", "Taitila", "Gara", "Vanija",
+    "Vishti", "Kintughna", "Shakuni", "Catushpada", "Naga"
+};
