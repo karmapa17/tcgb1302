8f9ceb3700bb2c83d765990a97ee36420f64e300
kmsheng <kmsh3ng@gmail.com>
2018-01-09 23:11:17 +0800

    add set_lang function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "main.h"
+
+void set_lang(void);
 
 int main()
 {
     printf("Hello World");
+    set_lang();
+}
+
+void set_lang(void)
+{
+    int i;
+    for (i = 0; i < 27; ++i) {
+      lunmanP[i] = lunmanT[i];
+    }
+    for (i = 0; i < 27; ++i) {
+      yogaP[i] = yogaT[i];
+    }
+    for (i = 0; i < 11; ++i) {
+      byedP[i] = byedT[i];
+    }
 }
