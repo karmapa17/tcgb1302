f508d41f14cb4d5bca1d0ccaf2651816f2ccda46
kmsheng <kmsh3ng@gmail.com>
2018-01-09 22:54:58 +0800

    update src folder

--- src/main.c
+++ src/main.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
-#include "t1.c"
-#include "t2.c"
-#include "t2a.c"
-#include "t3.c"
-#include "t4.c"
-#include "t4a.c"
-#include "t5.c"
-#include "utils.c"
-#include "tests.c"
-#include "bcd.c"
 
 int main()
 {
